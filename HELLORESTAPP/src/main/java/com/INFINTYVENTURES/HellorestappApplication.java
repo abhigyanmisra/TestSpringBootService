01148c1adf183d75d95d21bee6f7883329741551
baghelanuj72 <baghelanuj72@gmail.com>
2022-02-15 22:38:38 +0530

    greeting services done

---
+++ HELLORESTAPP/src/main/java/com/INFINTYVENTURES/HellorestappApplication.java
@@ -0,0 +1,14 @@
+package com.INFINTYVENTURES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HellorestappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellorestappApplication.class, args);
+	}
+
+}
---
+++ HELLORESTAPP/src/test/java/com/INFINTYVENTURES/HellorestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.INFINTYVENTURES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellorestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
